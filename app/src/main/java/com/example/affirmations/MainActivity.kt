d6719aea2cab08a1c535888061ffeda2987c0c84
Naimjr10 <njr848262@gmail.com>
2022-12-19 15:33:07 +0700

    reformat code

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.DataSource
+import com.example.affirmations.model.Affirmation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text: TextView = findViewById(R.id.text)
-        text.text = DataSource().loadAffirmation().size.toString()
+        val myDataset: List<Affirmation> = DataSource().loadAffirmation()
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
